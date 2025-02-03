79c630d2bbb835ad0880e5abe45aa301b2933845
v4r13d3s <20233L001041@utcv.edu.mx>
2025-02-03 14:04:59 -0600

    Actualización, implementación de cards y un carrusel de imagenes en la página home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,6 +6,10 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @NgModule({
   imports: [
@@ -14,6 +18,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], 
 })
 export class HomePageModule {}
