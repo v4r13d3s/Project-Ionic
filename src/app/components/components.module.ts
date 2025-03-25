76f2a4e811cb951bc1952673152164dc948a3e1f
v4r13d3s <20233L001041@utcv.edu.mx>
2025-03-25 13:16:15 -0600

    Implementación correcta del menu inferior

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuInferiorComponent } from './menu-inferior/menu-inferior.component';
-import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
 import { ItemChipComponent } from './item-chip/item-chip.component';
 import { IonicModule } from '@ionic/angular';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
@@ -12,7 +11,6 @@ import { DatePickerModal } from './date-picker-modal/date-picker-modal.component
 @NgModule({
   declarations: [
     MenuInferiorComponent,
-    MenuLateralComponent,
     ItemChipComponent,
     ItemChipProductComponent,
     ItemProductComponent,
@@ -25,7 +23,6 @@ import { DatePickerModal } from './date-picker-modal/date-picker-modal.component
   ],
   exports: [
     MenuInferiorComponent,
-    MenuLateralComponent,
     ItemChipComponent,
     ItemChipProductComponent,
     ItemProductComponent,
