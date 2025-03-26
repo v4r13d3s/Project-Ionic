ad5d1adc9dc98ccd1401c46b7a1283e9e6a00885
v4r13d3s <20233L001041@utcv.edu.mx>
2025-03-26 11:32:13 -0600

    Actualización en categorias

--- src/app/categorias/categorias.page.ts
+++ src/app/categorias/categorias.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+interface Category {
+  name: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-categorias',
@@ -8,9 +14,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriasPage implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [
+    { name: 'Papel', icon: 'https://cdn-icons-png.flaticon.com/512/448/448173.png' },
+    { name: 'Escolares', icon: 'https://cdn-icons-png.flaticon.com/512/1546/1546945.png' },
+    { name: 'Cuadernos', icon: 'https://cdn-icons-png.flaticon.com/512/3683/3683305.png' },
+    { name: 'Escritura', icon: 'https://cdn-icons-png.flaticon.com/512/12695/12695315.png' },
+    { name: 'Oficina', icon: 'https://cdn-icons-png.flaticon.com/512/1142/1142618.png' },
+    { name: 'Arte', icon: 'https://cdn-icons-png.flaticon.com/512/2970/2970785.png' },
+    { name: 'Impresion', icon: 'https://cdn-icons-png.flaticon.com/512/1339/1339350.png' },
+    { name: 'Manualidad', icon: 'https://cdn-icons-png.flaticon.com/512/3813/3813691.png' },
+    { name: 'Accesorios', icon: 'https://cdn-icons-png.flaticon.com/512/479/479063.png' },
+    { name: 'Regalos', icon: 'https://cdn-icons-png.flaticon.com/512/7813/7813840.png' },
+    { name: 'Archivo', icon: 'https://cdn-icons-png.flaticon.com/512/716/716784.png' }
+  ];
 
-  ngOnInit() {
+  constructor(private navCtrl: NavController) {}
+
+  ngOnInit() {}
+
+  selectCategory(category: Category) {
+    // Navigate to the specific category page or perform an action
+    console.log('Selected category:', category.name);
+    // Example navigation:
+    // this.navCtrl.navigateForward(`/category/${category.name.toLowerCase()}`);
   }
-
 }
